c9ad17656d6844070d4e6f21dc62e4dfe021e397
dhruvamalik123 <dhruvamalik.05@gmail.com>
2024-04-23 11:47:10 +0530

    initial commit

--- src/com/datastructure/sort/insertionsort.java
+++ src/com/datastructure/sort/insertionsort.java
@@ -1,4 +1,28 @@
 package com.datastructure.sort;
 
+import java.util.Arrays;
+//its adaptive, as the steps get reduced if the first part of the array is sorted
+//stable
+//good for small arrays and partially sorted arrays
 public class insertionsort {
+	public static void main(String[] args){
+		int[] arr={3,1,-2,5,-4};
+		for(int i=0;i<arr.length-1;i++){
+//			System.out.println(Arrays.toString(arr));
+			for(int j=i+1;j>0;j--){
+//				System.out.println(arr[j]+" "+arr[j-1]);
+				if (arr[j]<arr[j-1]){
+					int temp=arr[j-1];
+					arr[j-1]=arr[j];
+					arr[j]=temp;
+				}
+				else{
+					break;
+				}
+				System.out.println(Arrays.toString(arr));
+			}
+
+		}
+		System.out.println(Arrays.toString(arr));
+	}
 }
