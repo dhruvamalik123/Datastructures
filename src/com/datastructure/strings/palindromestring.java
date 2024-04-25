0921e404189897e9042d208e8193e594751dd1da
dhruvamalik123 <dhruvamalik.05@gmail.com>
2024-04-25 12:51:49 +0530

    new

--- src/com/datastructure/strings/palindromestring.java
+++ src/com/datastructure/strings/palindromestring.java
@@ -2,12 +2,15 @@ package com.datastructure.strings;
 
 public class palindromestring {
 	public static void main(String[] args){
-		String str="malayaala";
+		String str="";
 		String res=palindrom(str);
 		System.out.println(res);
 	}
 
 	public static String palindrom(String str) {
+		if(str.length()==0){
+			return "Its a palindrome";
+		}
 		for(int i=0;i<=str.length()/2;i++){
 			if(str.charAt(i)!=str.charAt(str.length()-i-1)){
 				return "Not a palindrome";
